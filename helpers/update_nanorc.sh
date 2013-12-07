a23f1c982c7c72e908b9393ba071f1aae8daa008
Dejan Fajfar <dejan@fajfar.com>
2013-12-07 23:25:44 +0100

    removed the not used et-stuff.nanorc file and corrected the update_nanorc.sh file to reflect the structure change

--- helpers/update_nanorc.sh
+++ helpers/update_nanorc.sh
@@ -1,9 +1,22 @@
 #! bin/bash
 
-rm .nanorc
-touch .nanorc
+# Path to the .nanorc file
+export nanorc_file="../.nanorc"
 
-for f in *.nanorc 
+# Empty the existing .nanorc file to prevent
+# contamination from a previous run
+if [ -f $nanorc_file ]
+then
+	# if.nanorc exists then only empty it
+	:> $nanorc_file
+else
+	# if .nanorc was not found then create a new one
+	touch .nanorc
+fi
+
+# For each *.nanorc
+for f in ../*.nanorc 
 do
-	echo "include \"~/.nano/$f\"" 
-done > .nanorc
+	file_name=${f:3}
+	echo "include \"~/.nano/$file_name\"" 
+done > $nanorc_file
